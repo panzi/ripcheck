11df7152d6f9139522c71ef40856eb5a76f35235
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2013-10-09 01:10:07 +0200

    added toolchain files for cross compilation

---
+++ contrib/toolchain-linux32.cmake
@@ -0,0 +1,10 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# Which compilers to use for C and C++
+set(CMAKE_C_COMPILER gcc -m32)
+set(CMAKE_CXX_COMPILER g++ -m32)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
