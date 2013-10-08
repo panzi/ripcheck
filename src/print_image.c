cc73adb9c8c6fceb437dc436d0c5ee48bc824934
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2013-10-08 04:35:20 +0200

    added license info

--- src/print_image.c
+++ src/print_image.c
@@ -1,3 +1,21 @@
+/**
+ *  ripcheck - find potential ripping errors in WAV files
+ *  Copyright (C) 2013  John Buckman of Magnatune, Mathias Panzenböck
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "print_text.h"
 #include "print_image.h"
 
--- src/ripcheck.c
+++ src/ripcheck.c
@@ -1,15 +1,20 @@
-/*********************************************************************
-    ripcheck.c
-    
-    from:
-    http://blog.magnatune.com/2013/09/ripcheck-detect-defects-in-cd-rips.html
-
-    written by John Buckman of Magnatune, Mathias Panzenböck
-
-    Released under GPL v3 license
-    http://www.gnu.org/licenses/gpl.html
-         
-*********************************************************************/
+/**
+ *  ripcheck - find potential ripping errors in WAV files
+ *  Copyright (C) 2013  John Buckman of Magnatune, Mathias Panzenböck
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <errno.h>
 #include <strings.h>
